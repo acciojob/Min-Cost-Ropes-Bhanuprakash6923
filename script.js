7b73ceee6c65ed095daba8c75327a809a327676c
Bhanuprakash6923 <158007213+Bhanuprakash6923@users.noreply.github.com>
2025-05-13 13:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,72 +1,27 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(val) {
-    this.heap.push(val);
-    this._bubbleUp();
-  }
-
-  extractMin() {
-    if (this.heap.length === 1) return this.heap.pop();
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this._bubbleDown();
-    return min;
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  _bubbleUp() {
-    let idx = this.heap.length - 1;
-    while (idx > 0) {
-      let parentIdx = Math.floor((idx - 1) / 2);
-      if (this.heap[parentIdx] <= this.heap[idx]) break;
-      [this.heap[parentIdx], this.heap[idx]] = [this.heap[idx], this.heap[parentIdx]];
-      idx = parentIdx;
-    }
-  }
-
-  _bubbleDown() {
-    let idx = 0;
-    const length = this.heap.length;
-    while (true) {
-      let leftIdx = 2 * idx + 1;
-      let rightIdx = 2 * idx + 2;
-      let smallest = idx;
-
-      if (leftIdx < length && this.heap[leftIdx] < this.heap[smallest]) {
-        smallest = leftIdx;
-      }
-      if (rightIdx < length && this.heap[rightIdx] < this.heap[smallest]) {
-        smallest = rightIdx;
-      }
-      if (smallest === idx) break;
-      [this.heap[idx], this.heap[smallest]] = [this.heap[smallest], this.heap[idx]];
-      idx = smallest;
-    }
-  }
-}
-
+4.
 function mincost(arr) {
   if (arr.length <= 1) return 0;
 
-  const heap = new MinHeap();
-  for (let rope of arr) {
-    heap.insert(rope);
-  }
+  // Min heap using a priority queue pattern
+  const heap = [...arr];
+  heap.sort((a, b) => a - b); // initial heapify
 
   let totalCost = 0;
-  while (heap.size() > 1) {
-    let first = heap.extractMin();
-    let second = heap.extractMin();
-    let cost = first + second;
+
+  while (heap.length > 1) {
+    // Pop two smallest elements
+    const first = heap.shift();
+    const second = heap.shift();
+
+    const cost = first + second;
     totalCost += cost;
-    heap.insert(cost);
+
+    // Insert the combined rope back into the heap
+    heap.push(cost);
+    heap.sort((a, b) => a - b); // Re-heapify
   }
 
   return totalCost;
 }
+
+module.exports = mincost;
